2ff8e003f5182be8a72bd152c7f502e9e2c0fa64
Jacob Leonard <jwleona2@asu.edu>
2017-02-23 18:31:51 -0700

    TG-72: TaskDesc can no longer be empty

--- src/net/sf/memoranda/ui/NewTaskWindow.java
+++ src/net/sf/memoranda/ui/NewTaskWindow.java
@@ -318,7 +318,7 @@ public class NewTaskWindow extends JFrame implements ActionListener {
 		}
 	
 		// CHECK TASK DESCRIPTION
-		if(taskDesc.getText() == null || taskDesc.getText().equals("Enter Task Description here...") || taskDesc.getText().equals("Task Description is invalid")){
+		if(taskDesc.getText() == null || taskDesc.getText().equals("") || taskDesc.getText().equals("Enter Task Description here...") || taskDesc.getText().equals("Task Description is invalid")){
 			System.out.println("Task Description is Invalid");
 			taskDesc.setText("Task Description is invalid");
 			statusLabel.setText("Invalid Entry: Please fix marked fields");
