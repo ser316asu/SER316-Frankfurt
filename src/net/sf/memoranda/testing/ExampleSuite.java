6a122dfe3c60e97661ab710ee27f0de9433adea4
trevorforrey <trevor4e@gmail.com>
2017-02-28 10:34:10 -0700

    US-61 & US-62 Finish Polish on build fix

--- src/net/sf/memoranda/testing/ExampleSuite.java
+++ src/net/sf/memoranda/testing/ExampleSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TaskCardTest.class})
+@SuiteClasses({TaskCardTest.class, JNCalendarPanelTest.class})
 public class ExampleSuite {
 
 	@BeforeClass
